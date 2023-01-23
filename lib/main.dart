9f41211dbf029865bc7e3874f2e9e39c63f69446
Lans12345 <olanalans12345@gmail.com>
2023-01-23 10:14:21 +0800

    fix depreciated

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,14 @@ import 'package:class_scheduling_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomeScreen(),
     );
   }
