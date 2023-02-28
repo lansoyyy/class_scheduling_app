e6cd309b193dc6965fb3baadbfef59807e460442
Lans12345 <olanalans12345@gmail.com>
2023-02-28 18:45:15 +0800

    fetched local datasource

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -107,177 +107,187 @@ class _CalendarScreenState extends State<CalendarScreen> {
                 builder: (context) {
                   return StatefulBuilder(builder: ((context, setState) {
                     return Dialog(
-                      child: Container(
-                        height: 420,
-                        decoration: BoxDecoration(
-                          color: primary,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 15, right: 15),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              TextBold(
-                                  text: 'Adding Event/Schedule',
-                                  fontSize: 18,
-                                  color: Colors.black),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              TextRegular(
-                                  text: 'Event name',
-                                  fontSize: 12,
-                                  color: Colors.black),
-                              SizedBox(
-                                height: 35,
-                                child: TextFormField(
-                                  onChanged: ((value) {
-                                    className = value;
-                                  }),
-                                  decoration: const InputDecoration(
-                                      enabledBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                            width: 1,
-                                            color: Colors.black), //<-- SEE HERE
-                                      ),
-                                      filled: true,
-                                      fillColor: Colors.white),
+                      child: SingleChildScrollView(
+                        child: Container(
+                          height: 420,
+                          decoration: BoxDecoration(
+                            color: primary,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 15, right: 15),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const SizedBox(
+                                  height: 20,
                                 ),
-                              ),
-                              const SizedBox(height: 10),
-                              TextRegular(
-                                  text: 'Schedule:',
-                                  fontSize: 12,
-                                  color: Colors.black),
-                              Center(
-                                child: SizedBox(
-                                  height: 50,
-                                  width: 300,
-                                  child: CupertinoDatePicker(
-                                    mode: CupertinoDatePickerMode.date,
-                                    initialDateTime: DateTime.now(),
-                                    onDateTimeChanged: (DateTime newDateTime) {
-                                      setState(() {
-                                        startingMonth = newDateTime.month;
-                                        startingDate = newDateTime.day;
-                                      });
-
-                                      // Handle the change
-                                    },
-                                  ),
+                                TextBold(
+                                    text: 'Adding Event/Schedule',
+                                    fontSize: 18,
+                                    color: Colors.black),
+                                const SizedBox(
+                                  height: 10,
                                 ),
-                              ),
-                              Center(
-                                child: SizedBox(
-                                  height: 50,
-                                  width: 300,
-                                  child: CupertinoDatePicker(
-                                    mode: CupertinoDatePickerMode.time,
-                                    initialDateTime: DateTime.now(),
-                                    onDateTimeChanged: (DateTime newDateTime) {
-                                      setState(() {
-                                        startHour = newDateTime.hour;
-                                        startMinutes = newDateTime.minute;
-                                      });
-                                      // Handle the change
-                                    },
-                                  ),
-                                ),
-                              ),
-                              Center(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 10, bottom: 10),
-                                  child: TextRegular(
-                                      text: 'to',
-                                      fontSize: 12,
-                                      color: Colors.black),
-                                ),
-                              ),
-                              Center(
-                                child: SizedBox(
-                                  height: 50,
-                                  width: 300,
-                                  child: CupertinoDatePicker(
-                                    mode: CupertinoDatePickerMode.time,
-                                    initialDateTime: DateTime.now(),
-                                    onDateTimeChanged: (DateTime newDateTime) {
-                                      setState(() {
-                                        endHour = newDateTime.hour;
-                                        endMinutes = newDateTime.minute;
-                                      });
-                                      // Handle the change
-                                    },
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(height: 30),
-                              Center(
-                                child: MaterialButton(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(5),
-                                    ),
-                                    color: greenAccet,
-                                    minWidth: 150,
-                                    height: 40,
-                                    child: TextRegular(
-                                        text: 'Done',
-                                        fontSize: 14,
-                                        color: Colors.white),
-                                    onPressed: () {
-                                      final id =
-                                          db.collection('Sched').doc().id;
-                                      db.collection('Sched').doc(id).set({
-                                        'event': className,
-                                        'startMonth': startingMonth,
-                                        'startDay': startingDate,
-                                        'startHour': startHour,
-                                        'startMinute': startMinutes,
-                                        'endHour': endHour,
-                                        'endMinute': endMinutes,
-                                        'id': id
-                                      });
-
-                                      _events.add(CalendarEventData(
-                                        date: DateTime(
-                                            2023, startingMonth, startingDate),
-                                        title: className,
-                                        endTime: DateTime(2023, startingMonth,
-                                            startingDate, endHour, endMinutes),
-                                        color: Colors.blue,
-                                        startTime: DateTime(
-                                            2023,
-                                            startingMonth,
-                                            startingDate,
-                                            startHour,
-                                            startMinutes),
-                                        endDate: DateTime(
-                                            2023, startingMonth, startingDate),
-                                      ));
-
-                                      Navigator.of(context).pushReplacement(
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  CalendarScreen()));
-
-                                      CalendarControllerProvider.of(context)
-                                          .controller
-                                          .addAll(_events);
-
-                                      // FlutterAlarmClock
-                                      //     .createAlarm(
-
-                                      //         startHour,
-                                      //         startMinutes,
-                                      //         title:
-                                      //             'Schedule: ${className}');
+                                TextRegular(
+                                    text: 'Event name',
+                                    fontSize: 12,
+                                    color: Colors.black),
+                                SizedBox(
+                                  height: 35,
+                                  child: TextFormField(
+                                    onChanged: ((value) {
+                                      className = value;
                                     }),
-                              ),
-                            ],
+                                    decoration: const InputDecoration(
+                                        enabledBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              width: 1,
+                                              color:
+                                                  Colors.black), //<-- SEE HERE
+                                        ),
+                                        filled: true,
+                                        fillColor: Colors.white),
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                TextRegular(
+                                    text: 'Schedule:',
+                                    fontSize: 12,
+                                    color: Colors.black),
+                                Center(
+                                  child: SizedBox(
+                                    height: 50,
+                                    width: 300,
+                                    child: CupertinoDatePicker(
+                                      mode: CupertinoDatePickerMode.date,
+                                      initialDateTime: DateTime.now(),
+                                      onDateTimeChanged:
+                                          (DateTime newDateTime) {
+                                        setState(() {
+                                          startingMonth = newDateTime.month;
+                                          startingDate = newDateTime.day;
+                                        });
+
+                                        // Handle the change
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                Center(
+                                  child: SizedBox(
+                                    height: 50,
+                                    width: 300,
+                                    child: CupertinoDatePicker(
+                                      mode: CupertinoDatePickerMode.time,
+                                      initialDateTime: DateTime.now(),
+                                      onDateTimeChanged:
+                                          (DateTime newDateTime) {
+                                        setState(() {
+                                          startHour = newDateTime.hour;
+                                          startMinutes = newDateTime.minute;
+                                        });
+                                        // Handle the change
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 10, bottom: 10),
+                                    child: TextRegular(
+                                        text: 'to',
+                                        fontSize: 12,
+                                        color: Colors.black),
+                                  ),
+                                ),
+                                Center(
+                                  child: SizedBox(
+                                    height: 50,
+                                    width: 300,
+                                    child: CupertinoDatePicker(
+                                      mode: CupertinoDatePickerMode.time,
+                                      initialDateTime: DateTime.now(),
+                                      onDateTimeChanged:
+                                          (DateTime newDateTime) {
+                                        setState(() {
+                                          endHour = newDateTime.hour;
+                                          endMinutes = newDateTime.minute;
+                                        });
+                                        // Handle the change
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 30),
+                                Center(
+                                  child: MaterialButton(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      color: greenAccet,
+                                      minWidth: 150,
+                                      height: 40,
+                                      child: TextRegular(
+                                          text: 'Done',
+                                          fontSize: 14,
+                                          color: Colors.white),
+                                      onPressed: () {
+                                        final id =
+                                            db.collection('Sched').doc().id;
+                                        db.collection('Sched').doc(id).set({
+                                          'event': className,
+                                          'startMonth': startingMonth,
+                                          'startDay': startingDate,
+                                          'startHour': startHour,
+                                          'startMinute': startMinutes,
+                                          'endHour': endHour,
+                                          'endMinute': endMinutes,
+                                          'id': id
+                                        });
+
+                                        _events.add(CalendarEventData(
+                                          date: DateTime(2023, startingMonth,
+                                              startingDate),
+                                          title: className,
+                                          endTime: DateTime(
+                                              2023,
+                                              startingMonth,
+                                              startingDate,
+                                              endHour,
+                                              endMinutes),
+                                          color: Colors.blue,
+                                          startTime: DateTime(
+                                              2023,
+                                              startingMonth,
+                                              startingDate,
+                                              startHour,
+                                              startMinutes),
+                                          endDate: DateTime(2023, startingMonth,
+                                              startingDate),
+                                        ));
+
+                                        Navigator.of(context).pushReplacement(
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    CalendarScreen()));
+
+                                        CalendarControllerProvider.of(context)
+                                            .controller
+                                            .addAll(_events);
+
+                                        // FlutterAlarmClock
+                                        //     .createAlarm(
+
+                                        //         startHour,
+                                        //         startMinutes,
+                                        //         title:
+                                        //             'Schedule: ${className}');
+                                      }),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
